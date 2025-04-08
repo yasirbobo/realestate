84b5aa1a41dd87ba97e8ee858299ff5dfed61ded
Mymummy123$ <samuelbrain14@gmail.com>
2025-04-08 13:28:36 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    fontFamily: {
+      serif: ['Ubuntu', 'sans-serif'],
+      sans: ['Open Sans', 'sans'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
